89e4a6dcfd5f5292929596de0e25bc8609a65ead
Birhanu G. Muleta <birhanugebisa@gmail.com>
2022-06-03 20:59:07 +0300

    replace element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# birhanu
+
+
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
