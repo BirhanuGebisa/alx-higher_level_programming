baecd93d09b5aba2717e23402b6129f699fbfeb5
Birhanu G. Muleta <birhanugebisa@gmail.com>
2022-06-08 23:26:22 +0300

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared = []
-    for line in matrix:
-        squared.append([c**2 for c in line])
-    return sqaured
-    return [[i ** 2 for i in row] for row in matrix]
+    square = []
+    for a in matrix:
+        square.append(list(map(lambda a: a ** 2, a)))
+    return (square)
