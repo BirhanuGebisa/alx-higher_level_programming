76d0a79fd94f2222fa84c2e3756be30bbdc09d09
Birhanu G. Muleta <birhanugebisa@gmail.com>
2022-06-08 23:28:46 +0300

    elements set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-# 3-common_elements.py
-# birhanu g
-
-
 def common_elements(set_1, set_2):
-    """Return a set of common elements in two sets."""
-    return (set_1 & set_2)
-
+    return(set_1.intersection(set_2))
