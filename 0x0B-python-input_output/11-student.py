7820be8aa6767be9752a75504cd0148f2e53a770
Birhanu G. Muleta <birhanugebisa@gmail.com>
2022-07-05 20:36:31 +0300

    stud

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+'''task 10 module'''
 
 
 class Student:
-    """Representation of a student"""
+    '''student class'''
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        '''Initialization of the student object'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
+        '''return a dictionary representation of a Student instance
+        If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        '''
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
             return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+        '''this function replaces all attributes of the Student instance
+        with the ones in the json argument
+        '''
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
