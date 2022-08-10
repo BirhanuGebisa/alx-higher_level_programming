4fd0127349e5d73920744b7622749bed36967831
Birhanu G. Muleta <birhanugebisa@gmail.com>
2022-08-10 23:37:56 +0300

    lists all genres in the database rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,4 +3,11 @@
 --    Results must be sorted in descending order by their rating
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
-IGNORE ALL;
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
