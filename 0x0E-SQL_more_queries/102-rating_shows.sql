5c00ad7eec045f09719a31abfee8962e8144256f
Birhanu G. Muleta <birhanugebisa@gmail.com>
2022-08-10 23:39:03 +0300

    The database name will be passed

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,4 +3,9 @@
 --    Results must be sorted in descending order by the rating
 --    You can use only one SELECT statement
 --    The database name will be passed as an argument of the mysql command
-IGNORE ALL;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
