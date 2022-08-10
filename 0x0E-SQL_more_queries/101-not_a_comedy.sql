32d7529f333e9b0e7b7d451c40556d9ab236ac29
Birhanu G. Muleta <birhanugebisa@gmail.com>
2022-08-10 23:40:09 +0300

    Comedy in the database

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,4 +4,14 @@
 --    Results must be sorted in ascending order by the show title
 --    You can use a maximum of two SELECT statement
 --    The database name will be passed as an argument of the mysql command
-IGNORE ALL;
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN
+(SELECT tv_shows.id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy")
+ORDER BY tv_shows.title;
